106e442c2a2555a4398a0862cfac4f7274d621bf
David Hu <davidhu314@gmail.com>
2017-02-22 08:32:50 -0800

    add default state to reducer template

--- templates/reducer.js
+++ templates/reducer.js
@@ -1,7 +1,9 @@
 const reducer = name => (
 `import { merge } from 'lodash';
 
-const ${name}Reducer = (state, action) => {
+_defaultState = {}
+
+const ${name}Reducer = (state = {}, action) => {
   Object.freeze(state);
   switch(action.type) {
     default:

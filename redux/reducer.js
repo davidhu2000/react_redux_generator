ab75235ccea611c12899a47e2639decae5cdddcb
David Hu <davidhu314@gmail.com>
2017-02-26 09:47:07 -0800

    add semicolon to import statement in root reducer

--- redux/reducer.js
+++ redux/reducer.js
@@ -39,7 +39,7 @@ const updateRootReducer = (nameLCC, nameSC) => {
 
       let importStr = 'import { combineReducers } from \'redux\';\n'
       let iIdx = root.indexOf(importStr) + importStr.length;
-      root = root.slice(0, iIdx) + `import ${nameLCC}Reducer from './${nameSC}_reducer.js'\n` + root.slice(iIdx);
+      root = root.slice(0, iIdx) + `import ${nameLCC}Reducer from './${nameSC}_reducer.js;'\n` + root.slice(iIdx);
 
       fs.writeFileSync('root_reducer.js', root);
     }

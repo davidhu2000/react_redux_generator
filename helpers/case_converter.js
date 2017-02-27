59991496ceaf9d2458d503736ff5632187bf753e
David Hu <davidhu314@gmail.com>
2017-02-27 08:26:53 -0800

    update case converter help into es6 format

--- helpers/case_converter.js
+++ helpers/case_converter.js
@@ -1,31 +1,37 @@
-const caseConverter = {
-  toSnakeCase: words => (
-    words.map( word => word.toLowerCase() ).join('_')
-  ),
-  toUpperCamelCase: words => (
-    words.map( word => word[0].toUpperCase() + word.slice(1) ).join('')
-  ),
-  toLowerCamelCase: words => (
-    words.map( (word, index) => {
-      if (index === 0) {
-        return word.toLowerCase();
-      } else {
-        return word[0].toUpperCase() + word.slice(1);
-      }
-    }).join('')
-  ),
-  toScreamingSnakeCase: (words) => (
-    words.map( word => word.toUpperCase() ).join('_')
-  ),
-  convert: (words, fs) => {
-    let wordsArray = words.split('_');
-    if (wordsArray.length === 1) {
-      wordsArray = words.split(/(?=[A-Z])/);
+const toSnakeCase = words => (
+  words.map( word => word.toLowerCase() ).join('_')
+);
+
+const toUpperCamelCase = words => (
+  words.map( word => word[0].toUpperCase() + word.slice(1) ).join('')
+);
+
+const toLowerCamelCase = words => (
+  words.map( (word, index) => {
+    if (index === 0) {
+      return word.toLowerCase();
+    } else {
+      return word[0].toUpperCase() + word.slice(1);
     }
-    return fs(wordsArray);
+  }).join('')
+);
+
+const toScreamingSnakeCase = words => (
+  words.map( word => word.toUpperCase() ).join('_')
+);
+
+const convert = (words, fs) => {
+  let wordsArray = words.split('_');
+  if (wordsArray.length === 1) {
+    wordsArray = words.split(/(?=[A-Z])/);
   }
+  return fs(wordsArray);
 };
 
-
-
-module.exports = caseConverter;
+module.exports = {
+  toSnakeCase,
+  toUpperCamelCase,
+  toLowerCamelCase,
+  toScreamingSnakeCase,
+  convert
+};

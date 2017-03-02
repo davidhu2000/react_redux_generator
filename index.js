540ec92d7d1e57ec218111e14e9e76a3b446f2bf
David Hu <davidhu314@gmail.com>
2017-03-02 10:44:55 -0800

    moved generator logic to own file

--- index.js
+++ index.js
@@ -4,12 +4,7 @@ require('shelljs/global');
 const logFunctions      = require('./helpers/logs.js');
 const helpTemplates     = require('./templates/help.js');
 
-const generateAction    = require('./fileTypes/action.js');
-const generateBase      = require('./fileTypes/base.js');
-const generateComponent = require('./fileTypes/component.js');
-const generateReducer   = require('./fileTypes/reducer.js');
-const generateStore     = require('./fileTypes/store.js');
-const generateUtil      = require('./fileTypes/util.js');
+const generator         = require('./commands/generator.js');
 
 let possibleTypes = ['reducers', 'stores', 'actions', 'utils', 'components', 'bases'].join('');
 let method        = process.argv[2];
@@ -30,29 +25,7 @@ if (['help', '--help', '-h'].includes(method)) {
   logFunctions.noNameErrorLog(type);
 
 } else {
-
   if(method === 'g' || method === 'generate') {
-
-    switch(type) {
-      case (type.match(/reducers?/) || {}).input:
-        generateReducer(name, actions);
-        break;
-      case (type.match(/stores?/) || {}).input:
-        generateStore();
-        break;
-      case (type.match(/actions?/) || {}).input:
-        generateAction(name, actions);
-        break;
-      case (type.match(/utils?/) || {}).input:
-        generateUtil(name, actions);
-        break;
-      case (type.match(/components?/) || {}).input:
-        generateComponent(name, actions);
-        break;
-      case (type.match(/bases?/) || {}).input:
-        generateBase(name);
-        generateStore();
-        break;
-    }
+    generator(type, name, actions)
   }
 }

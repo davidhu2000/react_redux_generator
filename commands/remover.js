8c8b52fb9a19ce12787816bc41905fdba0ed076b
David Hu <davidhu314@gmail.com>
2017-03-04 12:26:23 -0800

    modified remover to remove .js and .jsx files

--- commands/remover.js
+++ commands/remover.js
@@ -7,7 +7,12 @@ const createFilePath = (type, name) => {
     type = type.slice(0, type.length - 1);
   }
 
-  return `frontend/${type}s/${name}_${type}.js`;
+  let extension = 'js';
+  if (type === 'component') {
+    extension = 'jsx'
+  }
+
+  return `frontend/${type}s/${name}_${type}.${extension}`;
 }
 
 const deleteFile = (path) => {
@@ -23,7 +28,7 @@ const deleteFile = (path) => {
 
 const remover = (type, name) => {
   if( /bases?/.test(type) ) {
-
+    
   } else {
     path = createFilePath(type, name);
     deleteFile(path);

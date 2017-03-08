f245b716ae4c4d5d715d842a2773def65ddd2ec4
Raymond Lee <raylee0525@gmail.com>
2017-03-07 16:02:09 -0800

    Fixed semicolon issue in reducer.js

--- templates/base.js
+++ templates/base.js
@@ -30,7 +30,6 @@ const entryJSX = () => (
 `import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store.js';
-import Root from './components/root';
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');

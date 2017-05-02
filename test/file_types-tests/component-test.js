c193e6e96e31caa428894263f2c379a34351a38c
David Hu <davidhu314@gmail.com>
2017-05-02 12:40:56 -0700

    implemented component generate tests

---
+++ test/file_types-tests/component-test.js
@@ -0,0 +1,54 @@
+/* global rm */
+let chai = require('chai');
+let assert = chai.assert;
+let expect = chai.expect;
+let sinon = require('sinon');
+chai.use(require('chai-fs'));
+// require('shelljs');
+let Promise = require('bluebird');
+let rm = Promise.promisify(require('shelljs').rm);
+
+let generateComponent = Promise.promisify(require('../../file_types/component.js'));
+
+describe('Component Generator', () => {
+
+  before(() => {
+
+    // generateComponent('functional', ['-f']);
+    // generateComponent('noContainer', ['-nc']);
+  });
+
+  afterEach(() => {
+    Promise.resolve(rm('-r', 'frontend'));
+  });
+
+  it('should generate a component file and a container file', () => {
+    Promise.resolve(generateComponent('bothTypes', [])).then(() => {
+        expect('frontend/components/both_types/index.jsx').to.be.a.file();
+        expect('frontend/components/both_types/both_types.jsx').to.be.a.file();
+    });
+  });
+
+  it('should generate a function component file when receive -f flag', () => {
+    Promise.resolve(generateComponent('functional', ['-f'])).then(() => {
+      expect('frontend/components/functional/functional.jsx').to.be.a.file().with.content(`import React from 'react';
+
+const Functional = (props) => (
+
+);
+
+export default Functional;
+`);
+
+    });
+  });
+
+  it('should not generate a container file when receive the -nc flag', () => {
+    Promise.resolve(generateComponent('noContainer', ['-nc'])).then(() => {
+      expect('frontend/components/no_container/no_container.jsx').to.be.a.file();
+      expect('frontend/components/no_container/index.jsx').to.not.be.a.file();
+    });
+  });
+
+
+});
